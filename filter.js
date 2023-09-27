bac0e431c5816f1e9c2713d53e3ff0ce2f3d799d
rowanam <rowanamaas@gmail.com>
2023-09-27 17:41:44 +0000

    Initial commit

--- filter.js
+++ filter.js
@@ -1,4 +1,4 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node filter.js in the terminal
@@ -20,6 +20,18 @@ const people = [
     age: 18,
   },
 ];
+
+// Goal: filter people that are 18 or older
+const adults = people.filter(person => person.age >= 18);
+console.log(adults);
+
+// Goal: filter people that have names longer than 5 letters
+const longNames = people.filter(person => person.name.length > 5);
+console.log(longNames);
+
+// Goal: filter out Paul (/his object)
+const paul = people.filter(p => p.name === 'Paul')[0]; // since argument name is arbitrary, using p since it's shorter
+console.log(paul);
 
 
 // Complex Filtering
@@ -55,3 +67,56 @@ const students = [
     ]
   },
 ];
+
+// Goal: filter out job candidates who have at least 5 years of experience in at least one skill
+
+// My solution:
+const fiveYearsPlus = function (student) {
+  let candidate = false;
+
+  for (let skill of student.skills) {
+    if (skill.yrsExperience >= 5) {
+      candidate = true;
+    }
+  }
+
+  return candidate;
+}
+
+let candidates = students.filter(student => fiveYearsPlus(student) === true);
+console.log("\nCandidates, a:");
+console.log(candidates);
+
+// Instructor "inline" solution:
+const candidates1 = students.filter(student => {
+  let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+
+  return strongSkills.length > 0;
+})
+console.log("\nCandidates, b:");
+console.log(candidates1);
+
+// Refinement of my solution based on above:
+const fiveYearsPlus1 = function (student) {
+  for (let skill of student.skills) {
+    if (skill.yrsExperience >= 5) {
+      return true;
+    }
+  }
+}
+
+let candidates2 = students.filter(student => fiveYearsPlus1(student));
+console.log("\nCandidates, c:");
+console.log(candidates2);
+
+// Instructor separated functions solution:
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+
+const candidates3 = students.filter(hasStrongSkills);
+console.log("\nCandidates, d:");
+console.log(candidates3);
+
+// ...and simplified output:
+const names = candidates3.map(c => c.name);
+console.log(names);
